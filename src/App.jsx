d54c6c04553f4d6995a337f3234faf895f37e496
ts202 <ts2021864328@gmail.com>
2025-11-15 17:48:29 -0600

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 
 import { HashRouter, Route, Routes} from 'react-router'
 import './App.css'
-import AboutMe from './components/AboutMe'
-import Home from './components/Home'
-
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import MenuPage from "./pages/MenuPage";
+import CartPage from "./pages/CartPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import ConfirmationPage from "./pages/ConfirmationPage";
 function App() {
   // HashRouter:In URL, we do /# before the path.
   return <HashRouter> 
     <Routes>
-      <Route path= "/" element={<Home />}></Route>
-      <Route path= "/about" element={<AboutMe />}></Route>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/menu" element={<MenuPage cart={cart} setCart={setCart} />} />
+        <Route path="/cart" element={<CartPage cart={cart} setCart={setCart} />} />
+        <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
+        <Route path="/confirmation" element={<ConfirmationPage />} />
     </Routes>
   </HashRouter>
 }
